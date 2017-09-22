349bb9d1d2c71ffa585cb85abd201354a1f853f2
usuario <usuario@10.22.148.53>
2017-09-22 09:38:20 +0100

    Kata1: version2 se descha Date y se emplea Calendar.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,14 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
     
     public static void main(String[] args) {
-        Person yo=new Person("Luis",new Date(97,7,1));
+        Calendar fecha=GregorianCalendar.getInstance();
+        fecha.set(1997,7,1);
+        Person yo=new Person("Luis",fecha);
         System.out.println(yo.getName()+" tiene "+yo.getAge());
     }
 }
